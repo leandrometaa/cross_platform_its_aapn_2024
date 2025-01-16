0c295190aed02cf7c05c031fe87e032b9b8f2032
~venir <luca.venir@wde.it>
2025-01-16 11:32:06 +0100

    pokemon snippet logic

--- pokedex/lib/api/poke_api.dart
+++ pokedex/lib/api/poke_api.dart
@@ -1,6 +1,16 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pokedex/api/http_client.dart';
 import 'package:pokedex/api/models/poke_api_model.dart';
 import 'package:pokedex/api/models/poke_list_api_model.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part 'poke_api.g.dart';
+
+@riverpod
+PokeApi pokeApi(Ref ref) {
+  final client = ref.watch(httpClientProvider);
+  return PokeApi(client);
+}
 
 class PokeApi {
   const PokeApi(this.client);
--- pokedex/lib/api/poke_api.g.dart
+++ pokedex/lib/api/poke_api.g.dart
@@ -1,26 +1,26 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'http_client.dart';
+part of 'poke_api.dart';
 
 // **************************************************************************
 // RiverpodGenerator
 // **************************************************************************
 
-String _$httpClientHash() => r'8264ca1e861f6ea73b7604cffa7c3e5d12fdbd06';
+String _$pokeApiHash() => r'cd9db1712fb7a2cbaac6fd3c3651be907aad281b';
 
-/// See also [httpClient].
-@ProviderFor(httpClient)
-final httpClientProvider = AutoDisposeProvider<Dio>.internal(
-  httpClient,
-  name: r'httpClientProvider',
+/// See also [pokeApi].
+@ProviderFor(pokeApi)
+final pokeApiProvider = AutoDisposeProvider<PokeApi>.internal(
+  pokeApi,
+  name: r'pokeApiProvider',
   debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product') ? null : _$httpClientHash,
+      const bool.fromEnvironment('dart.vm.product') ? null : _$pokeApiHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
 @Deprecated('Will be removed in 3.0. Use Ref instead')
 // ignore: unused_element
-typedef HttpClientRef = AutoDisposeProviderRef<Dio>;
+typedef PokeApiRef = AutoDisposeProviderRef<PokeApi>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
